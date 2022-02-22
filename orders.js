3449f9e27bdaaa4588696c45ecb2522a06378c39
Luca Zilio de Oliveira <lucaoliveira846@gmail.com>
2022-02-22 13:29:26 -0300

    Dashboard Admin HTML/CSS/JS Responsive

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Name 1',
+        productNumber: 'Number 1',
+        paymentStatus: 'Payment Status 1',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Name 2',
+        productNumber: 'Number 2',
+        paymentStatus: 'Payment Status 2',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Name 3',
+        productNumber: 'Number 3',
+        paymentStatus: 'Payment Status 3',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Name 4',
+        productNumber: 'Number 4',
+        paymentStatus: 'Payment Status 4',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Name 5',
+        productNumber: 'Number 5',
+        paymentStatus: 'Payment Status 5',
+        shipping: 'Delivered'
+    },
+]
